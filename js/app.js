305ce3f199d0095a41da496cb2b6887b438d8c5d
imranahmed1990 <imranmarin1990@gmail.com>
2022-02-17 22:07:50 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -3,17 +3,14 @@ document.getElementById('calculator-button').addEventListener('click', function 
     const myIncomeInput = document.getElementById('monthly-income');
 
     const myIncomeAmt = myIncomeInput.value;
-
-
-    // console.log(myIncomeAmt)
-
     myIncomeInput.value = '';
+
     // my Expenes area coding in here...........
     const foodCost = document.getElementById('food-cost');
     const foodCostAmount = foodCost.value;
     const foodCostValue = parseFloat(foodCostAmount);
-    // console.log(foodCostAmount)
     foodCost.value = '';
+    // rant Input field
     const houseRant = document.getElementById('house-rant');
     const houseRantAmount = houseRant.value;
     const houseCost = parseFloat(houseRantAmount);
@@ -26,7 +23,6 @@ document.getElementById('calculator-button').addEventListener('click', function 
     // console.log(clotheAmount)
     clothesCost.value = '';
     const totalCost = foodCostValue + houseCost + clothCost;
-    // console.log(totalCost);
 
     // total Expances sum area...........
     const totalExpanses = document.getElementById('total-expances');
@@ -42,58 +38,10 @@ document.getElementById('calculator-button').addEventListener('click', function 
     const blanceText = blance.innerText;
     const blanceAmount = parseFloat(blanceText)
     const blanceWithoutSaving = blanceAmount + myIncomeAmt - totalCost;
-    blance.innerText = blanceAmount + myIncomeAmt - totalCost;
+    blance.innerText = blanceWithoutSaving;
 
     // console.log(blanceText)
 });
-
-// function calculator() {
-//     // get my income input field with function
-//     const myIncomeInput = document.getElementById('monthly-income');
-
-//     const myIncomeAmt = myIncomeInput.value;
-
-
-//     // console.log(myIncomeAmt)
-
-//     myIncomeInput.value = '';
-//     // my Expenes area coding in here...........
-//     const foodCost = document.getElementById('food-cost');
-//     const foodCostAmount = foodCost.value;
-//     const foodCostValue = parseFloat(foodCostAmount);
-//     // console.log(foodCostAmount)
-//     foodCost.value = '';
-//     const houseRant = document.getElementById('house-rant');
-//     const houseRantAmount = houseRant.value;
-//     const houseCost = parseFloat(houseRantAmount);
-//     houseRant.value = '';
-//     // console.log(houseRantAmount)
-//     const clothesCost = document.getElementById('clothes-cost');
-//     const clotheAmount = clothesCost.value;
-//     const clothCost = parseFloat(clotheAmount);
-
-//     // console.log(clotheAmount)
-//     clothesCost.value = '';
-//     const totalCost = foodCostValue + houseCost + clothCost;
-//     // console.log(totalCost);
-
-//     // total Expances sum area...........
-//     const totalExpanses = document.getElementById('total-expances');
-//     const previousTotalExp = totalExpanses.innerText;
-//     const previousExpancesTotal = parseFloat(previousTotalExp);
-
-
-//     totalExpanses.innerText = previousExpancesTotal + totalCost;
-
-//     // console.log(expansesTotal)
-
-//     const blance = document.getElementById('blance-amount');
-//     const blanceText = blance.innerText;
-//     const blanceAmount = parseFloat(blanceText)
-//     blance.innerText = blanceAmount + myIncomeAmt - totalCost;
-
-//     // console.log(blanceText)
-// }
 
 // saveing handle event listener
 document.getElementById('save-Button').addEventListener('click', function () {
@@ -105,12 +53,28 @@ document.getElementById('save-Button').addEventListener('click', function () {
     const previousBlanceAmount = perviousBlance.innerText;
     const previousTotal = parseFloat(previousBlanceAmount);
     const parsentageTotal = previousTotal * saveInput / 100;
-    console.log(parsentageTotal)
+    // console.log(parsentageTotal)
 
-
+    /* 
+    money saving codding area.
+    */
     const savingTotal = document.getElementById('seving-total');
     const savingTotalText = savingTotal.innerText;
     const finalSaving = parseFloat(savingTotalText);
     savingTotal.innerText = parsentageTotal;
+
+    // remaining blance amount in pocket
+    const lastBlance = document.getElementById('blance-amount');
+    const updateBlanceText = lastBlance.innerText;
+    const updateBlance = parseFloat(updateBlanceText)
+    const restAmount = updateBlance - parsentageTotal;
+    // console.log(restAmount);
+    const remainingBlance = document.getElementById('remaining-amount');
+    const remainingBlanceText = remainingBlance.innerText;
+    const remainingBlanceTotal = parseFloat(remainingBlanceText);
+    const pokectBlance = remainingBlanceTotal + restAmount;
+    remainingBlance.innerText = pokectBlance;
+
+
 
 });
